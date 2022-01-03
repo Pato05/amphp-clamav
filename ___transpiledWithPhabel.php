fb214609f9c1342ac22a6c5117d8bb4f09ed033b
Pato05 <20874747+Pato05@users.noreply.github.com>
2022-01-03 17:47:43 +0100

    namespace changes, add functions.php add phabel for retrocompatibility as well
    Release transpiled using https://phabel.io, the PHP transpiler

---
+++ ___transpiledWithPhabel.php
@@ -0,0 +1,3 @@
+<?php if (!getenv("PHABEL_INSIDE_COMPOSER")) die("pato05/amphp-clamav: This package requires transpilation using the phabel.io composer plugin.
+To use this package please run composer update, enabling execution of plugins and generation of the lockfile.
+");
